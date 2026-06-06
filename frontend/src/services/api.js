5268437cb6a9d868401745bf937bfac2fe1ffa77
MD ALI JAUHAR <jauharking786@gmail.com>
2026-06-06 22:47:20 +0530

    Update api.js
    import library

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,7 @@
+import axios from "axios";
+
 const API = axios.create({
-  baseURL: "https://social-media-app-51w2.onrender.com/api", // production
+  baseURL: "https://social-media-app-51w2.onrender.com/api",
 });
+
+export default API;
